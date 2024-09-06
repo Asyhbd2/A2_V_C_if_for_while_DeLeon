e8c271bea910e0b59b39d1d61bac8835a4d9f441
Angel Tadeo De Leon Ceniceros <a.22308051281165@cbtis128.edu.mx>
2024-09-06 09:32:44 -0600

    Add files via upload
    Subi dos archivos

---
+++ miscadenas.py
@@ -0,0 +1,22 @@
+'''Angel Tadeo
+trabajando con cadenas
+'''
+nombre="Tadeo" 
+print(nombre[0])
+print(nombre)
+
+MisNovias=["Twilight","Applejack","Fluttershy"]
+for chicas in MisNovias:
+    print(chicas) 
+
+print("Otro ejemplo")
+b = "Tadeo Ceniceros"
+print(b[2:5])
+print("A mayusculas")
+a = "Angel Tadeo"
+print(a.upper())
+print("Concatenar")
+a = "Maria"
+b = "Perez"
+c = a + " "+ b
+print(c)
